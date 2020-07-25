331ec682cda31372112d8a12e1b41008f8da6ddd
hesensen <hesensen@hztianque.com>
2020-07-26 01:23:09 +0800

    循环依赖【开关】

--- spring-hss/src/test/java/com/hss/test/Test.java
+++ spring-hss/src/test/java/com/hss/test/Test.java
@@ -11,6 +11,12 @@ public class Test {
 	public void beanTest(){
 		//初始化spring容器
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+		/*
+		//设置spring 不支持循环依赖
+		ac.setAllowCircularReferences(false);
+		//刷新容器，使配置生效
+		ac.refresh();
+		*/
 		IndexService indexService = ac.getBean(IndexService.class);
 //		UserService userService = ac.getBean(UserService.class);
 		indexService.getService();
